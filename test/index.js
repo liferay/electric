f48ad8f99adcafe132cf6474bf16f0a6d5ecbc8e
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2016-11-01 10:41:18 -0700

    Add soyweb test

--- test/index.js
+++ test/index.js
@@ -2,40 +2,38 @@
 
 var fs = require('fs-extra');
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var path = require('path');
 var test = require('ava');
 
 var runSequence = require('run-sequence').use(gulp);
 
 var registerTasks = require('../lib/index').registerTasks;
+var sitePath = path.join(__dirname, 'fixture/sites/static-site');
 
 test.before(function(t) {
+	process.chdir(sitePath);
+
 	registerTasks({
 		gulp: gulp
 	});
+
+	var tasks = gulp.tasks;
+
+	t.truthy(tasks.clean);
+	t.truthy(tasks['front-matter']);
+	t.truthy(tasks.generate);
+	t.truthy(tasks.metal);
+	t.truthy(tasks.soyweb);
 });
 
-test.cb.afterEach(function(t) {
+test.cb.after.always(function(t) {
 	runSequence('clean', function() {
 		t.end();
 	});
 });
 
-test('it should require all task files', function(t) {
-	var tasks = gulp.tasks;
-
-	t.ok(tasks.clean);
-	t.ok(tasks['front-matter']);
-	t.ok(tasks.generate);
-	t.ok(tasks.metal);
-	t.ok(tasks.soyweb);
-});
-
 test.cb('it should generate site.json file that matches pages folder structure', function(t) {
-	var sitePath = path.join(__dirname, 'fixture/sites/static-site');
-
-	process.chdir(sitePath);
-
 	runSequence('front-matter', function() {
 		var config = {encoding: 'utf8'};
 		var siteData = fs.readFileSync(path.join(sitePath, 'site.json'), config);
@@ -44,5 +42,19 @@ test.cb('it should generate site.json file that matches pages folder structure',
 		t.is(siteData, targetSiteData);
 
 		t.end();
+	});
+});
+
+test.cb('it should compile soyweb templates', function(t) {
+	runSequence('front-matter', 'soyweb', function() {
+		gulp.src('dist/**/*.html')
+			.pipe(gutil.buffer(function(err, files) {
+				t.is(path.relative(files[0].base, files[0].path), 'index.html');
+				t.is(path.relative(files[1].base, files[1].path), 'child/index.html');
+				t.is(files[0].contents.length, 552);
+				t.is(files[1].contents.length, 651);
+
+				t.end();
+			}));
 	});
 });
