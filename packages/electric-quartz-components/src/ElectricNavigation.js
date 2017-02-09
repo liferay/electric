8ebc09a82f7a53e7588d342ae309319bde052009
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-02-09 11:33:37 -0800

    Init commit

---
+++ packages/electric-quartz-components/src/ElectricNavigation.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import Soy from 'metal-soy';
+import {ElectricNavigation} from 'electric-base-components';
+
+import templates from './ElectricNavigation.soy';
+
+Soy.register(ElectricNavigation, templates);
+
+export default ElectricNavigation;
---
+++ packages/electric-quartz-components/src/all/components.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import ElectricCode from '../ElectricCode';
+import ElectricNavigation from '../ElectricNavigation';
+import ElectricReadingProgress from '../ElectricReadingProgress';
+import ElectricSearch from '../ElectricSearch';
+import ElectricSearchAutocomplete from '../ElectricSearchAutocomplete';
+import ElectricUpdates from '../ElectricUpdates';
+
+export {
+	ElectricCode,
+	ElectricNavigation,
+	ElectricReadingProgress,
+	ElectricSearch,
+	ElectricSearchAutocomplete,
+	ElectricUpdates
+};
+
+export default ElectricNavigation;
