8c65a779365cc83fb0d3d022e8f8afad17b5eb5b
Ryan Garant <ryantgarant@gmail.com>
2017-07-26 17:56:24 -0700

    Added test for rss generator

---
+++ test/fixtures/sites/rss-generator-site/electric.config.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+	rss: {
+		enabled: true,
+		description: 'This is the description of your blogs RSS feed',
+		domain: 'https://027ffbd9.ngrok.io',
+		managingEditor: 'Name of person who manages',
+		image_url: 'example.png',
+		webMaster: 'Name of person who manages',
+		copyright: '2017',
+		language: 'English',
+		ttl: 60
+	}
+};
