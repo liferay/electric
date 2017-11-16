75c7e5d34925c036808d801b0b2f689d9a9c3696
Zeno Rocha <zno.rocha@gmail.com>
2017-11-16 13:58:50 -0800

    Adds lerna configuration

---
+++ jest-electric-lerna-resolver.js
@@ -0,0 +1,11 @@
+var resolve = require('resolve');
+
+module.exports = function(file, data) {
+  if (file.indexOf('electric-') === 0) {
+    data.packageFilter = function(package) {
+      return { main: package['jsnext:main'] };
+    };
+  }
+
+  return resolve.sync(file, data);
+};
