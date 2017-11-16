8fa9bdbf25cf7b5c3d79de3ce0606374987be6d0
Zeno Rocha <zno.rocha@gmail.com>
2017-11-16 14:14:02 -0800

    Migrates electric-code-tabs to monorepo

---
+++ packages/electric-code-tabs/gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var metal = require('gulp-metal');
+
+metal.registerTasks({
+	bundleCssFileName: 'electric-code-tabs.css',
+	bundleFileName: 'electric-code-tabs.js',
+	moduleName: 'electric-code-tabs'
+});
---
+++ packages/electric-code-tabs/test/ElectricCodeTabs.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import CodeTabs from '../src/ElectricCodeTabs';
+
+describe('CodeTabs', function() {
+	it('should be tested', function() {
+		assert.fail('No tests for this module yet.');
+	});
+});
