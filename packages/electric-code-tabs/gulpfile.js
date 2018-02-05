ca918eb8119829c3dbab5af53e63704d2d9283b5
Zeno Rocha <zno.rocha@gmail.com>
2018-02-05 15:07:32 -0800

    Removes electric-code-tabs since that is already inside of electric-base-components

--- packages/electric-code-tabs/gulpfile.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var metal = require('gulp-metal');
-
-metal.registerTasks({
-	bundleCssFileName: 'electric-code-tabs.css',
-	bundleFileName: 'electric-code-tabs.js',
-	moduleName: 'electric-code-tabs'
-});
--- packages/electric-code-tabs/test/ElectricCodeTabs.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import CodeTabs from '../src/ElectricCodeTabs';
-
-describe('CodeTabs', function() {
-	it('should be tested', function() {
-		assert.fail('No tests for this module yet.');
-	});
-});
